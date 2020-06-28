66d3dc904d09eb4c9e40310787f6624812d9ced7
Kevin Walton <notkevinwalton@gmail.com>
2020-06-28 09:31:14 -0400

    2020.06.28-09.31.14

--- minecraft_bedrock.sh
+++ minecraft_bedrock.sh
@@ -6,6 +6,9 @@
 # All directory references are relative to BEDROCK_SERVER_DIR.   #
 ##################################################################
 BEDROCK_SERVER_DIR="/opt/minecraft_bedrock"
+USERNAME="notkevinwalton@gmail.com"
+PASSWORD="7oub$GIT*%^"
+GIT_URL="https://$USERNAME:$PASSWORD@github.com/IronWalnut/minecraft.git"
 
 
 ######## Run GitHub Backup ########
@@ -19,7 +22,7 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 git commit -m $current_time
 
 # Push to GitHub
-git push origin master
+git push  master
 
 
 ######## Start Minecraft Bedrock Server ########
