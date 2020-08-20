2efaea2530b52c6e8eefac315aa053f705213bb0
Kevin Walton <notkevinwalton@gmail.com>
2020-08-20 17:56:55 -0400

    Fix script so it actually backs up the world files

--- minecraft_bedrock.sh
+++ minecraft_bedrock.sh
@@ -6,7 +6,7 @@
 # All directory references are relative to BEDROCK_SERVER_DIR.   #
 ##################################################################
 BEDROCK_SERVER_DIR='/opt/minecraft_bedrock'
-REPO_DIR="$BEDROCK_SERVER_DIR/worlds"
+LD_LIBRARY_PATH="$BEDROCK_SERVER_DIR"
 
 ######## Wait for networking stuff to come online ########
 sleep 60
@@ -14,12 +14,10 @@ sleep 60
 ######## Run Server GitHub Backup ########
 # Create commit on all files with timestamp as message
 CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
-cd $REPO_DIR && git commit -a -m "$CURRENT_TIME"
+cd $BEDROCK_SERVER_DIR && git add -A && git commit -a -m "$CURRENT_TIME"
 
 # Push to GitHub using SSH (as root)
-cd $REPO_DIR && git push origin master -v
+cd $BEDROCK_SERVER_DIR && git push origin master -v
 
 ######## Start Minecraft Bedrock Server ########
-cd $BEDROCK_SERVER_DIR
-LD_LIBRARY_PATH="$BEDROCK_SERVER_DIR"
-sudo ./bedrock_server
+cd $BEDROCK_SERVER_DIR && sudo ./bedrock_server
