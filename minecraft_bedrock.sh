c27a3fea6b8aae31441442bd17a3bb38a72053bd
Kevin Walton <notkevinwalton@gmail.com>
2020-08-22 09:58:21 -0400

    2020.08.22-09.58.21

--- minecraft_bedrock.sh
+++ minecraft_bedrock.sh
@@ -9,15 +9,15 @@ BEDROCK_SERVER_DIR='/opt/minecraft_bedrock'
 LD_LIBRARY_PATH="$BEDROCK_SERVER_DIR"
 
 ######## Wait for networking stuff to come online ########
-sleep 120
+sleep 60
 
 ######## Run Server GitHub Backup ########
 # Create commit on all files with timestamp as message
 CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
-cd $BEDROCK_SERVER_DIR && git add -A && git commit -a -m "$CURRENT_TIME"
+cd $BEDROCK_SERVER_DIR && sudo git add -A && sudo git commit -a -m "$CURRENT_TIME"
 
 # Push to GitHub using SSH (as root)
-cd $BEDROCK_SERVER_DIR && git push origin master -v
+cd $BEDROCK_SERVER_DIR && sudo git push origin master -v
 
 ######## Start Minecraft Bedrock Server ########
 cd $BEDROCK_SERVER_DIR && sudo ./bedrock_server
