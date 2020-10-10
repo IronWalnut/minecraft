53c452aa561ce74d4b2f8e32a68832a56458e33e
Kevin Walton <notkevinwalton@gmail.com>
2020-10-10 14:31:56 -0400

    Update script to not care about repo path

--- minecraft_bedrock.sh
+++ minecraft_bedrock.sh
@@ -3,22 +3,26 @@
 ##################################################################
 # Notes: This script is run as sudo by crontab on server reboot. #
 # It will push a backup the world, then start the server.        #
-# All directory references are relative to BEDROCK_SERVER_DIR.   #
+# All directory references are relative to this script.          #
 ##################################################################
-BEDROCK_SERVER_DIR='/opt/minecraft_bedrock'
+BEDROCK_SERVER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LD_LIBRARY_PATH="$BEDROCK_SERVER_DIR"
-cd $BEDROCK_SERVER_DIR 
+cd $BEDROCK_SERVER_DIR
 
 ######## Wait for networking stuff to come online ########
+echo "Waiting 60 seconds..."
 sleep 60
 
 ######## Run Server GitHub Backup ########
 # Create commit on all files with timestamp as message
 CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Committing Changes..."
 sudo git add -A && sudo git commit -a -m "$CURRENT_TIME"
 
 # Push to GitHub using SSH (as root)
+echo "Pushing to Github..."
 sudo git push origin master -v
 
 ######## Start Minecraft Bedrock Server ########
+echo "Starting Server..."
 cd $BEDROCK_SERVER_DIR && sudo ./bedrock_server
